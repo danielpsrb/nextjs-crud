af44785946feade5fc3d2548bb57893e2f74c8d3
danielpsrb <danielpsrb01@gmail.com>
2024-07-08 22:29:44 +0700

    update libs for datas

--- libs/data.ts
+++ libs/data.ts
@@ -8,3 +8,35 @@ export const getCustomers = async () => {
         throw new Error("Failed to fetch customers data");
     }
 }
+
+export const getCustomerDataById = async (id: string) => {
+    try {
+        const customer = await prisma.guest.findUnique({
+            where: {
+                id: id
+            }
+        });
+        return customer;
+    } catch (error) {
+        throw new Error("Failed to fetch customer data");
+    }
+}
+
+export const getCustomerPages = async (query: string) => {
+    try {
+        const customers = await prisma.guest.count({
+            where: {
+                OR: [
+                    {
+                        name: {
+                            contains: query,
+                        }
+                        
+                    }
+                ]
+            }
+        })
+    } catch (e) {
+        
+    }
+}
