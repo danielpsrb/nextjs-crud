dd2a44049523aa1d3e8fb97a640422838268809d
danielpsrb <danielpsrb01@gmail.com>
2024-07-08 20:53:29 +0700

    update components buttons

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -1,6 +1,9 @@
+"use client"
+import Link from "next/link";
+import { useFormStatus } from "react-dom";
 import { FaEdit } from "react-icons/fa"
 import { MdDelete } from "react-icons/md";
-
+import clsx from "clsx";
 
 export const EditButton = () => {
     return (
@@ -17,3 +20,24 @@ export const DeleteButton = () => {
         </button>
     )
 }
+
+export const CreateCustomerButton = ({ label }: { label: string }) => {
+    const { pending } = useFormStatus();
+
+    const styled = clsx(
+        "btn btn-accent w-full text-white",
+        {
+            "opacity-50 cursor-progress": pending,
+        }
+    )
+
+    return (
+        <button type="submit" className={styled} disabled={pending}>
+            {label === "save" ? (
+                <span>{pending ? "Saving..." : "Submit"}</span>
+            ) : (
+            <span>{pending ? "Updating..." : "Update"}</span>
+        )}
+        </button>
+    )
+};
