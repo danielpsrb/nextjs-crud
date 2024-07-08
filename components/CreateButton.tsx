7581c60394b52b7c0701f5cfc2502509f164eca7
danielpsrb <danielpsrb01@gmail.com>
2024-07-08 20:54:09 +0700

    update route link create button

--- components/CreateButton.tsx
+++ components/CreateButton.tsx
@@ -3,7 +3,7 @@ import { IoAddSharp } from "react-icons/io5"
 
 export const CreateButton = () => {
     return (
-        <Link href="/contacts/new" className="inline-flex items-center space-x-1 text-gray-100 bg-blue-700 m-2 px-5 py-[9px] rounded-md text-sm">
+        <Link href="/customer/create" className="inline-flex items-center space-x-1 text-gray-100 bg-blue-700 m-2 px-5 py-[9px] rounded-md text-sm">
             <IoAddSharp size={20} />
             Create
         </Link>
